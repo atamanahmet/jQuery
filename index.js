1261775d4e4932a7ca29343d1cd33bd23b26d6af
atamanahmet <ahmtataman@proton.me>
2024-09-14 13:33:27 +0300

    modified:   index.html 	modified:   index.js 	modified:   styles.css

---
+++ index.js
@@ -0,0 +1,12 @@
+$("h1").css("color", "red")
+$("#btn2").css("font-size","5rem")
+
+if($("h1").text()=="Title"){
+console.log(true);
+}
+else{
+    console.log(false);
+}
+console.log($("h1").text());
+$("Button").html("<em>Click Me!</em>");
+
