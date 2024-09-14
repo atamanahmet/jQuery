97c3b846095326505a12ad7f1b1bbd157d0e724f
atamanahmet <ahmtataman@proton.me>
2024-09-14 15:00:48 +0300

    modified:   index.html 	modified:   index.js 	new file:   sounds/blue.mp3 	new file:   sounds/green.mp3 	new file:   sounds/red.mp3 	new file:   sounds/wrong.mp3 	new file:   sounds/yellow.mp3 	modified:   styles.css

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-$("h1").css("color", "red")
-$("#btn2").css("font-size","5rem")
+// $("h1").css("color", "red")
+// $("#btn2").css("font-size","5rem")
 
-if($("h1").text()=="Title"){
-console.log(true);
-}
-else{
-    console.log(false);
-}
-console.log($("h1").text());
-$("Button").html("<em>Click Me!</em>");
+// if($("h1").text()=="Title"){
+// console.log(true);
+// }
+// else{
+//     console.log(false);
+// }
+// console.log($("h1").text());
+// $("Button").html("<em>Click Me!</em>");
 
-console.log($("button").attr("class"));
+// console.log($("button").attr("class"));
+// var sentence = "";
+// $(document).keypress(function (event) 
+// { 
+//     sentence = sentence + event.key;
+    
+// $("h1").text(sentence);
+// });
 
