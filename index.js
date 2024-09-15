63eb27f56e4db38a7580b4af859a33d0fcf9eeea
atamanahmet <ahmtataman@proton.me>
2024-09-15 10:45:33 +0300

    modified:   index.js 	modified:   styles.css

--- index.js
+++ index.js
@@ -12,34 +12,75 @@
 
 // console.log($("button").attr("class"));
 // var sentence = "";
-// $(document).keypress(function (event) 
-// { 
+// $(document).keypress(function (event)
+// {
 //     sentence = sentence + event.key;
-    
+
 // $("h1").text(sentence);
 // });
+
 var randomNum;
 var btnClick;
-console.log(randomNum);
+var userArray = [];
+var randomArray = [];
+var randomCheck;
+var counter = 0;
 
-$("button").click(inputCount);
+$("body").keypress(function () {
+  randomNum = Math.floor(Math.random() * 4 + 1);
+  randomArray.push(randomNum);
+  randomFlash();
+  // console.log("Random number is " + randomNum);
+  console.log(randomArray);
 
+  $("button").click(function () {
 
-function inputCount(){
-    randomNum = Math.floor(Math.random()*4+1)
-    btnClick = $(this).attr("id");
-    $(this).addClass("flash");
-    setTimeout(() =>{
-        
-        $(this).removeClass("flash");
-        
-    },100);
+    btnClick = Number($(this).attr("id"));
+    userArray.push(btnClick);
+    // console.log("counter base array "+userArray[counter]);
+    addFlash();
+
     
-   
-    console.log(btnClick);
-    if (randomNum==btnClick) {
-        //continue
-    } else {
-        // $("body").css("background-color", "red")
+
+    for (i=0; i<counter; i++){
+  
+      if(randomArray[i]==userArray[i]){
+        // continue 
+        console.log("same");
+        console.log("User Array = "+userArray);
+        console.log("random Array = "+randomArray);
+      }
+      else if (randomArray[i]!=userArray[i]){
+        // red
+        console.log("not same");
+        
+      }
     }
+    // console.log("User Select " + btnClick);
+    randomNum = Math.floor(Math.random() * 4 + 1);
+    randomArray.push(randomNum);
+    // console.log(randomArray);
+    randomFlash();
+    // console.log("Random number is " + randomNum);
+    counter++;
+    console.log("counter "+counter);
+  });
+});
+
+function addFlash() {
+  $(this).addClass("flash");
+
+  setTimeout(() => {
+    $(this).removeClass("flash");
+  }, 100);
 }
+
+function randomFlash() {
+  randomCheck = "#" + randomNum;
+  $(randomCheck).addClass("activeBox");
+  setTimeout(() => {
+    $(randomCheck).removeClass("activeBox");
+  }, 700);
+}
+
+
