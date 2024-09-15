97bc01bdd8de562ba313b7d6349b636f044a5d9c
atamanahmet <ahmtataman@proton.me>
2024-09-15 13:30:14 +0300

    modified:   index.js 	modified:   styles.css

--- index.js
+++ index.js
@@ -42,10 +42,8 @@
 //     // console.log("counter base array "+userArray[counter]);
 //     addFlash();
 
-    
-
 //     for (i=0; i<counter; i++){
-  
+
 //     }
 //     // console.log("User Select " + btnClick);
 //     randomNum = Math.floor(Math.random() * 4 + 1);
@@ -75,29 +73,79 @@
 // }
 
 var randomNumber;
-var gameArray=[];
-var userArray=[];
+var gameArray = [];
+var userArray = [];
 var userSelection;
-var gameCounter=0;
+var gameCounter = 0;
+var clickCounter = 0;
+var soundCount;
+var soundObj = {
+  sound0 : new Audio("./sounds/green.mp3"),
+  sound1 : new Audio("./sounds/red.mp3"),
+  sound2 : new Audio("./sounds/yellow.mp3"),
+  sound3 : new Audio("./sounds/blue.mp3"),
+  wrong : new Audio("./sounds/wrong.mp3")
+}
 
-$("body").keypress(function (){
- randomNumber= Math.floor(Math.random()*4);
- gameArray.push(randomNumber);
- console.log(gameArray);
 
- $("button").click(function (){
-  userSelection = Number($(this).attr("id"));
-  userArray.push(userSelection);
-  console.log(gameArray);
-  console.log(userArray);
-  if (userArray[gameCounter]==gameArray[gameCounter]){
-    console.log("same");
-    gameCounter++;
-  }
-  else {
-    console.log("wrong");
-  }
- })
+$("body").keypress(function () {
+  generateAndPushRandom();
+  
+  $("button").click(function () {
+   soundCount ="sound"+$(this).attr("id");
+   console.log(soundCount);
+   soundObj[soundCount].play();
+   $(this).addClass("flash");
+   
+
+    userSelection = Number($(this).attr("id"));
+    userArray.push(userSelection);
+
+    console.log(gameArray);
+
+
+    if (gameArray[clickCounter] == userSelection) {
+      console.log("user selection ="+userSelection);
+      console.log("GameRandom = "+gameArray[clickCounter]);
+      console.log("same");
+      generateAndPushRandom();
+      userArray.push(userSelection);
+      clickCounter++;
+    } else if (gameArray[clickCounter] != userSelection) {
+      console.log("user selection ="+userSelection);
+      console.log("GameRandom = "+gameArray[clickCounter]);
+      console.log("user Array = "+userArray);
+      gameOver();
+    }
+  });
 });
 
-
+function generateAndPushRandom() {
+  randomNumber = Math.floor(Math.random() * 4);
+  gameArray.push(randomNumber);
+  $("#"+randomNumber).animate(
+    {
+      opacity: 0.25,
+    },
+    100,
+    function () {
+      
+      $("#"+randomNumber).animate(
+        {
+          opacity: 1,
+        },
+        100
+      ); // Animation complete.
+    }
+  );
+  
+  
+  console.log(gameArray[clickCounter]);
+  
+}
+function gameOver() {
+  console.log("wrong");
+  soundObj.wrong.play();
+  $("body").css("background-color", "red");
+  $("h1").html("Game Over.<br>Press F5 to Restart");
+}
