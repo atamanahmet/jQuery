86f48abca64d57fbbf9eaeb69f20810a09bf3fa1
atamanahmet <ahmtataman@proton.me>
2024-09-18 10:29:17 +0300

    modified:   index.js

--- index.js
+++ index.js
@@ -72,122 +72,124 @@
 //   }, 700);
 // }
 
-var randomNumber;
-var gameArray = [];
-var userArray = [];
-var userSelection;
-var counter = -1;
-var clickCounter = 0;
-var level = 0;
-var started= false;
-var soundCount;
-var soundObj = {
-  sound0: new Audio("./sounds/green.mp3"),
-  sound1: new Audio("./sounds/red.mp3"),
-  sound2: new Audio("./sounds/yellow.mp3"),
-  sound3: new Audio("./sounds/blue.mp3"),
-  wrong: new Audio("./sounds/wrong.mp3"),
-};
 
-$("body").keypress(function () {
-  if(!started){
-  generateAndPushRandom();
-  $("h1").text("Level "+level)
-  $("button").click(function () {
-    clickCounter++;
-    // Sound-Animation etc
-    soundCount = "sound" + $(this).attr("id");
-    soundObj[soundCount].play();
+// //Dice Game
+// var randomNumber;
+// var gameArray = [];
+// var userArray = [];
+// var userSelection;
+// var counter = -1;
+// var clickCounter = 0;
+// var level = 0;
+// var started= false;
+// var soundCount;
+// var soundObj = {
+//   sound0: new Audio("./sounds/green.mp3"),
+//   sound1: new Audio("./sounds/red.mp3"),
+//   sound2: new Audio("./sounds/yellow.mp3"),
+//   sound3: new Audio("./sounds/blue.mp3"),
+//   wrong: new Audio("./sounds/wrong.mp3"),
+// };
+
+// $("body").keypress(function () {
+//   if(!started){
+//   generateAndPushRandom();
+//   $("h1").text("Level "+level)
+//   $("button").click(function () {
+//     clickCounter++;
+//     // Sound-Animation etc
+//     soundCount = "sound" + $(this).attr("id");
+//     soundObj[soundCount].play();
     
-    $(this).addClass("flash");
-    setTimeout(() => {
-      $(this).removeClass("flash");
-    }, 200);
+//     $(this).addClass("flash");
+//     setTimeout(() => {
+//       $(this).removeClass("flash");
+//     }, 200);
 
-    //User button click input#id getting and pushing into an array
-    userSelection = Number($(this).attr("id"));
+//     //User button click input#id getting and pushing into an array
+//     userSelection = Number($(this).attr("id"));
 
-    userArray.push(userSelection);
-    // console.log("object");
-    // userArray.push(userSelection);
+//     userArray.push(userSelection);
+//     // console.log("object");
+//     // userArray.push(userSelection);
 
-    // console.log(gameArray);
-    if (gameArray[counter] == userSelection) {
-      console.log(true);
-      counter++;
-      if (clickCounter == gameArray.length) {
-        console.log("click counter " + clickCounter);
-        statusCheck();
-      }
-    } else {
-      gameOver();
-    }
+//     // console.log(gameArray);
+//     if (gameArray[counter] == userSelection) {
+//       console.log(true);
+//       counter++;
+//       if (clickCounter == gameArray.length) {
+//         console.log("click counter " + clickCounter);
+//         statusCheck();
+//       }
+//     } else {
+//       gameOver();
+//     }
     
 
-    // if (JSON.stringify.gameArray == JSON.stringify.userArray) {
-    //   generateAndPushRandom();
-    // }
-    //   if ((gameArray.length == clickCounter)&&(gameArray == userArray)) {
-    //     generateAndPushRandom();
-    // }
-  });
-}});
+//     // if (JSON.stringify.gameArray == JSON.stringify.userArray) {
+//     //   generateAndPushRandom();
+//     // }
+//     //   if ((gameArray.length == clickCounter)&&(gameArray == userArray)) {
+//     //     generateAndPushRandom();
+//     // }
+//   });
+// }});
 
-function generateAndPushRandom() {
- started=true;
-  randomNumber = Math.floor(Math.random() * 4);
-  gameArray.push(randomNumber);
-  level++;
-  $("#" + randomNumber).animate(
-    {
-      opacity: 0.25,
-    },
-    300,
-    function () {
-      $("#" + randomNumber).animate(
-        {
-          opacity: 1,
-        },
-        300
-      ); // Animation complete.
-    }
-  );
-  counter++;
-  // console.log(gameArray[clickCounter]);
-  console.log(gameArray);
-}
-function gameOver() {
-  console.log("wrong");
-  soundObj.wrong.play();
-  $("body").css("background-color", "red");
-  setTimeout(()=> {
-    $("body").css("background-color", "rgb(0, 0, 53)")
-  },400)
+// function generateAndPushRandom() {
+//  started=true;
+//   randomNumber = Math.floor(Math.random() * 4);
+//   gameArray.push(randomNumber);
+//   level++;
+//   $("#" + randomNumber).animate(
+//     {
+//       opacity: 0.25,
+//     },
+//     300,
+//     function () {
+//       $("#" + randomNumber).animate(
+//         {
+//           opacity: 1,
+//         },
+//         300
+//       ); // Animation complete.
+//     }
+//   );
+//   counter++;
+//   // console.log(gameArray[clickCounter]);
+//   console.log(gameArray);
+// }
+// function gameOver() {
+//   console.log("wrong");
+//   soundObj.wrong.play();
+//   $("body").css("background-color", "red");
+//   setTimeout(()=> {
+//     $("body").css("background-color", "rgb(0, 0, 53)")
+//   },400)
     
   
-  $("h1").html("Game Over. Press F5");
-  started=false;
-  // console.log("user selection "+ userSelection);
-  // console.log("user selection "+ gameArray[counter]);
-}
-function statusCheck() {
-  for (i = 0; i < gameArray.length; i++) {
-    if (gameArray[i] == userArray[i]) {
-      //continue
-      console.log("check done.");
-      generateAndPushRandom();
-      clickCounter = 0;
-      counter=0;
-      gameArray=[];
-      level=0;
-      userArray = [];
-      $("h1").text("Level "+level)
-    } 
-    // else {
-    //   // gameOver();
-    // }
-  }
-}
-function startOver(){
+//   $("h1").html("Game Over. Press F5");
+//   started=false;
+//   // console.log("user selection "+ userSelection);
+//   // console.log("user selection "+ gameArray[counter]);
+// }
+// function statusCheck() {
+//   for (i = 0; i < gameArray.length; i++) {
+//     if (gameArray[i] == userArray[i]) {
+//       //continue
+//       console.log("check done.");
+//       generateAndPushRandom();
+//       clickCounter = 0;
+//       counter=0;
+//       gameArray=[];
+//       level=0;
+//       userArray = [];
+//       $("h1").text("Level "+level)
+//     } 
+//     // else {
+//     //   // gameOver();
+//     // }
+//   }
+// }
+// function startOver(){
 
-}
+// }
