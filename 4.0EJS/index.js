3104b6e57f082226cd644da6c3203c436a3bfa46
atamanahmet <ahmtataman@proton.me>
2024-09-21 13:52:47 +0300

    modified:   4.0EJS/index.js 	modified:   4.0EJS/views/index.ejs

--- 4.0EJS/index.js
+++ 4.0EJS/index.js
@@ -6,14 +6,14 @@ import path from "path";
 const port = 3000;
 const app = express();
 const indexDirectory = new URL(fileURLToPath(path.dirname(import.meta.url)));
-// const directoryEJS = indexDirectory.pathname + "/views/index.ejs";
+const directoryEJS = indexDirectory.pathname + "/views/index.ejs";
 const date = new Date();
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 var day = weekDays[date.getDay()];
 app.set('view engine', 'ejs')
-console.log(day);
+console.log(directoryEJS);
 
-console.log(indexDirectory.pathname);
+// console.log(indexDirectory.pathname);
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -24,11 +24,7 @@ app.listen(port, (req, res) => {
   console.log("Listening on port: " + port);
 });
 app.post("/daywrite", (req, res) => {
-  res.render("index.ejs", {name: "ataman"}, (err) => {
-    // res.sendFile(indexDirectory + "/index.html");
-    if(err){
-    console.log(err);}
-  });
+  res.render("index.ejs", {name: req.body["name"]});
   console.log(req.body.name1);
  
 });
