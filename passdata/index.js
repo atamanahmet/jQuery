90240b8141a09e80e8d543220598dd97420ed05b
atamanahmet <ahmtataman@proton.me>
2024-09-22 11:45:33 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   passdata/index.js

--- passdata/index.js
+++ passdata/index.js
@@ -3,4 +3,14 @@ import fileURLtoPath from "url";
 
 const port = 3000;
 const app = express();
+var data;
 
+app.get("/", (req, res) => {
+    res.render("index.ejs", data)
+})
+app.post("/submit", (req, res) => {
+    
+})
+app.listen(port, (req, res) => {
+    console.log("Server listening on port : "+port);
+})
