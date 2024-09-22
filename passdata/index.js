24296fcd1eedf610ace0e56d65fae2ed21a0b66c
atamanahmet <ahmtataman@proton.me>
2024-09-22 12:42:01 +0300

    modified:   passdata/index.js 	modified:   passdata/views/index.ejs

--- passdata/index.js
+++ passdata/index.js
@@ -3,15 +3,25 @@ import fileURLtoPath from "url";
 
 const port = 3000;
 const app = express();
-var data;
+var strCount;
 
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
-})
+  res.render("index.ejs");
+});
+
 app.post("/submit", (req, res) => {
-    
-})
+res.locals.strCount = (req.body.name + req.body.lastname).trim().length;
+// console.log(strCount);
+  res.render("index.ejs", strCount);
+  
+});
+
 app.listen(port, (req, res) => {
-    console.log("Server listening on port : "+port);
-})
+  console.log("Server listening on port : " + port);
+});
+
+function countChar() {
+ strCount = (data.name + data.lastname).trim().length;
+}
